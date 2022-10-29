3c079abd1de5eeea55fa67129b4a25b63ba2887c
Kirill <me4iniy@gmail.com>
2022-10-29 18:28:27 +0500

    Добавьте файлы проекта.

---
+++ GroupNumberFourNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroupNumberFour
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine();
+        }
+    }
+}
