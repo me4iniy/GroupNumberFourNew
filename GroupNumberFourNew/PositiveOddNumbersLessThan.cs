2384ad3dbe36fce3ea7d5c89a6f72d3c826cbad3
Kirill <me4iniy@gmail.com>
2022-10-29 18:38:48 +0500

    Done

---
+++ GroupNumberFourNew/PositiveOddNumbersLessThan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupNumberFourNew
+{
+    public class PositiveOddNumbersLessThan
+    {
+        public static void PositiveOddNumbersLessThanResult()
+        {
+            int sumOddNumber = 0;
+
+            for (int i = 0; i < 50; i++)
+                if (i % 2 != 0)
+                    sumOddNumber += sumOddNumber + i;
+
+            Console.WriteLine(sumOddNumber);
+        }
+    }
+}
